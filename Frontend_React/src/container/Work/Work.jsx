5d83de0d0e384814b8be95c4f11fe312a4d4eca5
Huey-dev <matthewokadinya@gmail.com>
2023-08-18 15:04:02 +0100

    comments

--- Frontend_React/src/container/Work/Work.jsx
+++ Frontend_React/src/container/Work/Work.jsx
@@ -6,6 +6,27 @@ import { urlFor, client } from '../../client'
 import './Work.scss'
 
 const Work = () => {
+  const [activeFilter, setActiveFilter] = useState("All")
+  const [animateCard, setAnimateCard] = useState({y: 0, opacity: 1})
+  const [works, setWorks] = useState([])
+  const [FilterWork, setFilterWork] = useState([])
+
+  useEffect(() => {
+    const query = '*[_type == "works"]'
+    client.fetch(query)
+      .then((data) => {
+        // When the data is fetched successfully, update both 'works' and 'FilterWork' states.
+        // Update 'works' state with fetched data
+        setWorks(data)
+         // Update 'FilterWork' state with fetched data
+        setFilterWork(data)
+      })
+  }, [])
+  
+
+  const handleWorkFilter = (item) => {
+
+  }
   return (
     <>
       <h2 className='head-text'>
@@ -13,8 +34,34 @@ const Work = () => {
         <span> Portfolio </span>
          Section
       </h2>
+      <div className='app__work-filter'>
+        {['UI/UX', 'Web App', 'Mobile App', 'React JS', 'All'].map((item, index) => (
+          <div
+            key={index}
+            className={`app__work-filter app__flex ${activeFilter === item ? 'item-active': '' }`}
+            onClick={() => handleWorkFilter(item)}
+          >
+            {item}
+          </div>
+
+          
+        ))}
+      </div>
+      <motion.div
+        animate={animateCard}
+        transition={{duration:0.5, delayChildren:0.5}}
+        className='app__work-portfolio'
+      >
+        {FilterWork.map((work, index) => (
+          <div className='app__work-item app__flex' key={index} >
+            <div className='app__work-img app__flex'>
+              <img src={urlFor(work.imgUrl)} alt={work.name} />
+            </div>
+          </div>
+        ))}
+      </motion.div>
     </>
   )
 }
 
-export default Work
+export default AppWrap (Work, 'work')
