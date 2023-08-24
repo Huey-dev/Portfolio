8dfbb22297df3628a1895badffc53425c3445192
Huey-dev <matthewokadinya@gmail.com>
2023-08-24 05:21:55 +0100

    testimonial

--- Frontend_React/src/container/Testimonial/Testimonial.jsx
+++ Frontend_React/src/container/Testimonial/Testimonial.jsx
@@ -1,6 +1,25 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { motion} from 'framer-motion'
+import { HiChevronLeft, HiChevronRight } from 'react-icons/hi'
+import {AppWrap} from '../../wrapper'
+import { urlFor, client } from '../../client'
 import './Testimonial.scss'
+
 const Testimonial = () => {
+
+const [testimonials,  setTestimonals]  = useState([])
+const [currentIndex, setCurrentIndex] = useState(0)  
+
+useEffect(() => {
+ const query = '*[_type == "testimonials"]'
+
+  client.fetch(query)
+    .then((data) => {
+      setTestimonals(data)
+    })
+}, [])
+
+
   return (
     <div>
       Testimonial
@@ -8,4 +27,4 @@ const Testimonial = () => {
   )
 }
 
-export default Testimonial
+export default AppWrap(Testimonial, 'testimonial')
