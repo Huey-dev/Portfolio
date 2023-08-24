9c3283ae18a39cde5bc31a30bc8ef76300f01621
Huey-dev <matthewokadinya@gmail.com>
2023-08-24 12:07:59 +0100

    styling

--- Frontend_React/src/container/Testimonial/Testimonial.jsx
+++ Frontend_React/src/container/Testimonial/Testimonial.jsx
@@ -8,7 +8,11 @@ import './Testimonial.scss'
 const Testimonial = () => {
 
 const [testimonials,  setTestimonals]  = useState([])
-const [currentIndex, setCurrentIndex] = useState(0)  
+const [currentIndex, setCurrentIndex] = useState(0) 
+
+const handleClick = (index) => {
+  setCurrentIndex(index)
+}
 
 useEffect(() => {
  const query = '*[_type == "testimonials"]'
@@ -19,11 +23,39 @@ useEffect(() => {
     })
 }, [])
 
-
+const test = testimonials[currentIndex]
   return (
-    <div>
-      Testimonial
-    </div>
+    <>
+      {testimonials.length && (
+        <> 
+          <div className="app__testimonial-item app__flex">
+            <img src={urlFor(test.imgUrl)} alt="testimonial" />
+            <div className="app__testimonial-content">
+              <p className="p-text">{test.feedback}</p>
+              <div>
+                <h4 className="bold-text">{test.name}</h4>
+                <h5 className="p-text">{test.company}</h5>
+              </div>
+            </div>
+          </div>
+
+          <div className="app__testimonial-btns app__flex">
+            <div className="app__flex" onClick={() =>
+             handleClick(currentIndex === 0 
+              ? testimonials.lengthcurrentIndex - 1 
+                : currentIndex - 1)}>
+                  <HiChevronLeft/>
+            </div>
+            <div className="app__flex" onClick={() =>
+             handleClick(currentIndex ===  testimonials.length - 1
+              ? 0
+                : currentIndex + 1)}>
+                  <HiChevronRight/>
+            </div>
+          </div>
+        </>
+      )}
+    </>
   )
 }
 
