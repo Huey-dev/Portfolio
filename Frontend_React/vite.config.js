993a32c949fe56f3fdefda8486ce522908951a55
Matthew Okadinya <matthewokadinya@gmail.com>
2023-12-30 00:43:35 +0100

    n

--- Frontend_React/vite.config.js
+++ Frontend_React/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      input: 'src/main.jsx', // Specify the path to your entry JavaScript file
+      input: './src/main.jsx', // Specify the path to your entry JavaScript file
     },
   },
 })
